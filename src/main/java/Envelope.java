43f2aef309311982c63557876e719afa71b1f027
Aleksey <alekseyh215@gmail.com>
2019-10-14 16:40:08 +0400

    lesson 2

---
+++ src/main/java/Envelope.java
@@ -0,0 +1,28 @@
+
+public class Envelope {
+    public static void main(String[] args) {
+        int width = 20;
+        int height = 18;
+        double tan = ((double)width) / height;
+
+        // vertical loop
+        for (int i = 0; i < height/2; i++) {
+            // horizontal loop
+            for (int j = 0; j < width; j++) {
+                if (
+                        i == 0             // first horizontal line
+                        || i == height/2-1 // last horizontal line
+                        || j == 0          // left vertical line
+                        || j == width - 1  // right vertical line
+                        || j == (int)(i * tan)
+                        || (width-j-1)==(int)(i*tan)
+                ) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
--- src/main/java/lesson02/ArrayMultidimensional.java
+++ src/main/java/lesson02/ArrayMultidimensional.java
@@ -4,5 +4,12 @@ public class ArrayMultidimensional {
     public static void main(String[] args) {
         int[] a = {1,2,3};
         int[][] b = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(b.length);
+
+        int[][][] c =
+                {{{1, 2 ,3}
+
+                }
+
     }
 }
