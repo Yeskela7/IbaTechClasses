80555b26b62f15608c5b6cb96fbb0ecea1166a80
Aleksey <alekseyh215@gmail.com>
2019-10-23 16:37:03 +0400

    --- Arraylist

--- src/main/java/lesson5/figures/GeometricApp.java
+++ src/main/java/lesson5/figures/GeometricApp.java
@@ -1,28 +1,28 @@
-package lesson5.figures;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-public class GeometricApp {
-    public static void main(String[] args) {
-        new Square(new Point(1,1), new Point(2,2));
-//step one
-        ArrayList<Figure> figures = new ArrayList<Figure>();
-        for(int i = 0; i < 3; i++){
-            figures.add(new Circle());
-        }
-        for(int i = 0; i < 3; i++){
-            figures.add(new Square());
-        }
-        for(int i = 0; i < 3; i++){
-            figures.add(new Triangle());
-        }
-        for(int i = 0; i < 3; i++){
-            figures.add(new Rectangle());
-        }
-        int total = 0;
-        for(Figure f: figures)
-
-
-    }
-}
+//package lesson5.figures;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//
+//public class GeometricApp {
+//    public static void main(String[] args) {
+//        new Square(new Point(1,1), new Point(2,2));
+////step one
+//        ArrayList<Figure> figures = new ArrayList<Figure>();
+//        for(int i = 0; i < 3; i++){
+////            figures.add(new Circle());
+//        }
+//        for(int i = 0; i < 3; i++){
+//            figures.add(new Square());
+//        }
+//        for(int i = 0; i < 3; i++){
+//            figures.add(new Triangle());
+//        }
+//        for(int i = 0; i < 3; i++){
+//            figures.add(new Rectangle());
+//        }
+//        int total = 0;
+//
+//
+//
+//    }
+//}
