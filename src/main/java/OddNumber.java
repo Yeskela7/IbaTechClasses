b486b6fb0bf80059c2940177f1629db6c79a9555
yeskela <aleksey215@gmail.com>
2019-11-22 18:39:04 +0400

    --- lesson 18 (nov 22)

---
+++ src/main/java/OddNumber.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class OddNumber {
+    public static void main(String[] args) {
+
+
+
+    }
+    private static int getOdd(int i){
+        return 3*(i*2-1);
+    }
+}
